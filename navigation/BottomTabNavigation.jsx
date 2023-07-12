a00009bc7e6aca90a414b232d0b739c77c630a1e
Chijioke Chika <8pluscreative@gmail.com>
2023-07-12 18:53:59 +0100

    Bottom bar

---
+++ navigation/BottomTabNavigation.jsx
@@ -0,0 +1,78 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home, Profile, Search } from "../screens";
+// import Home from "../screens";
+// import Search from "../screens";
+// import Profile from "../screens";
+import { Iconicons } from "@expo/vector-icons";
+import { COLORS } from "../constants/index";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = {
+  tabBarShowLabels: false,
+  tabBarHideOnKeyboard: true,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    elevation: 0,
+    height: 70,
+  },
+};
+
+const BottomTabNavigation = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Iconicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Iconicons
+                name={"search-sharp"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Iconicons
+                name={focused ? "person" : "person-outline"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigation;

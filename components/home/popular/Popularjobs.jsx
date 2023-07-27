6c94615f333433b149419379370736ad9d4a09b6
Chijioke Chika <8pluscreative@gmail.com>
2023-07-27 06:47:00 +0100

    Home

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import { useState } from "react";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import styles from "./popularjobs.style";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
 
-export default Popularjobs
+      <View style={styles.cardsContainer}></View>
+    </View>
+  );
+};
+
+export default Popularjobs;

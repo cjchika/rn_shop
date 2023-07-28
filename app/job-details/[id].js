5abefa1d6dabb8ef02e8b2e602fdf3de6ba0fc0f
Chijioke Chika <8pluscreative@gmail.com>
2023-07-28 04:48:55 +0100

    Job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,7 +1,51 @@
-import React from "react";
+import { useState, useCallback } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
-const [id] = () => {
-  return <div>[id]</div>;
+const JobDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back}
+            />
+          ),
+        }}
+      >
+        JobDetails
+      </Stack.Screen>
+    </SafeAreaView>
+  );
 };
 
-export default [id];
+export default JobDetails;

8e8a456466d6b2e756c5247f33521ae31bc4f164
Chijioke Chika <8pluscreative@gmail.com>
2023-07-29 07:26:55 +0100

    Job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -63,17 +63,17 @@ const JobDetails = () => {
           {isLoading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
           ) : error ? (
-            <Text>Something went wrong.</Text>
-          ) : data?.length === 0 ? (
-            <Text>No Data</Text>
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data available</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
               <Company
-                companyLogo={data[0]?.employer_logo}
-                jobTitle={data[0]?.job_title}
-                companyName={data[0]?.employer_name}
-                location={data[0]?.job_country}
-              />{" "}
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
               <JobTabs />
             </View>
           )}
